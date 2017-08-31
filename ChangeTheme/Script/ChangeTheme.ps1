31e083527bd4530ba704c06f62411e725d4e45c3
aylwin <awong789@users.noreply.github.com>
2017-08-31 16:58:10 +1000

    Update ChangeTheme.ps1

--- ChangeTheme/Script/ChangeTheme.ps1
+++ ChangeTheme/Script/ChangeTheme.ps1
@@ -2,8 +2,9 @@ Add-PSSnapin Microsoft.Xrm.Tooling.Connector
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Import-Module Microsoft.Xrm.Data.Powershell
 
+$orgName = <organisation>
 $Cred = Get-Credential -UserName <username> -Message "Please Enter admin credentials for CRM"
-$conn = Get-CrmConnection -Credential $Cred -DeploymentRegion Oceania -OnlineType Office365 -OrganizationName <organisation>
+$conn = Get-CrmConnection -Credential $Cred -DeploymentRegion Oceania -OnlineType Office365 -OrganizationName $orgName 
 
 $req = New-Object Microsoft.Crm.Sdk.Messages.PublishThemeRequest
 $req.target = New-CrmEntityReference -EntityLogicalName "theme" -Id "<themeguid>"
